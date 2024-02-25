a339c326d59d0dac59fb7dca72733484c4472e25
Xpiral397 <111965825+Xpiral397@users.noreply.github.com>
2024-02-25 01:01:21 +0100

    Add files via upload
    all

---
+++ monitoring/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import MakeAnalysisView,MakeAnalysisPostView
+
+app_name = 'GeeAnalysis'
+
+urlpatterns = [
+    path('get-analysis/<str:user_id>/<str:analysis_name>/',  MakeAnalysisView.as_view(), name='get'),
+    path('make-analysis/<str:userId>/<str:analysisName>/', MakeAnalysisPostView.as_view(), name='post'),
+]
